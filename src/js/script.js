f9a43e64bca98f052d0797c0449a128b34befd83
jamesaking02 <jamesaking02@outlook.com>
2023-06-21 21:50:47 -0700

    added code 4 fixing the header to the top on scrl

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,19 @@ h1.forEach((heading) => {
 })
 
 const title = document.querySelectorAll(".creation__title")
+
+// Make header fixed on scroll
+const header = document.querySelector("header")
+const headerContainer = header.querySelector(".header-container")
+
+const headerIo = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      headerContainer.classList.remove("active")
+    } else {
+      headerContainer.classList.add("active")
+    }
+  })
+})
+
+headerIo.observe(header)
